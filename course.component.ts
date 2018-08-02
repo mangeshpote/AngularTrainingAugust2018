f302a3edf5cc40d90ab91d40d53f0f09aa66d876
mangeshpote <mr.pote1@gmail.com>
2018-08-02 19:12:46 +0530

    Quick start project ..
    Just a simple Component.. One BootstrapComponent - AppComponent.
    AppComponent is a parent component and CourseComponent is a child. Checkout app.component.ts, app.module.ts (check declarations) and course.component.ts and index.html (component tag rendered in body tag)

---
+++ course.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core"
+
+
+@Component({
+    selector:`course`, //this is a tag name in HTML
+    template:`
+    <h1>Angular</h1>
+    `
+    // template:`<my-app></my-app>`
+})
+export class CourseComponent{
+
+}
